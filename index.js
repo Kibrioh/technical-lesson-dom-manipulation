6df4e0a3454e9ee1afc066b1da8a2588895ccab3
Brian Kiprotich <briankiprotich602@gmail.com>
2026-04-30 18:25:26 +0300

    Dom manipulation

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 // Write your code here!
 
+const userInfo = {
+	username: "flatUser123",
+	description: "Hi, welcome to my webpage! Just a new user of this website!",
+	theme: "red"
+}
+
+function displayUser(userInfo){
+	const header = document.querySelector("#main")
+}
+
+displayUser(userInfo)
+
